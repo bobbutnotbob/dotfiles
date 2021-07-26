99daa1fb3f2ea5cf632b33705aafaa26722a3af6
Emile Reiser <emile.reiser@outlook.co.nz>
2021-07-27 01:01:29 +1200

    cleaned up pywal-sublime section

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -1,7 +1,16 @@
 #!/bin/zsh
 
+SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
+
 # Theme sublime text with pywal
-curl -o $HOME/.local/bin/sublime-pywal https://raw.githubusercontent.com/Brychlikov/pywal_sublime/master/pywal_sublime.py
-chmod +x $HOME/.local/bin/sublime-pywal
-rm $HOME/.config/sublime-text/Packages/User/Preferences.sublime-settings
-ln -s $HOME/dotfiles/config/sublime-text/Preferences.sublime-settings $HOME/.config/sublime-text/Packages/User
+SUBLIMEDIR="$HOME/.config/sublime-text/Packages/User"
+if [ -e "$SUBLIMEDIR/Preferences.sublime-settings" ] && [ ! -L "$SUBLIMEDIR/Preferences.sublime-settings" ]; then
+	#curl -o $HOME/.local/bin/sublime-pywal https://raw.githubusercontent.com/Brychlikov/pywal_sublime/master/pywal_sublime.py
+	#sed -i -e "s/sublime-text-3/sublime-text/g" $HOME/.local/bin/sublime-pywal
+	#chmod +x $HOME/.local/bin/sublime-pywal
+	#rm $HOME/.config/sublime-text/Packages/User/Preferences.sublime-settings
+	#ln -s $SCRIPTDIR/../config/sublime-text/Preferences.sublime-settings $HOME/.config/sublime-text/Packages/User
+	echo "pogchamp"
+fi
+
+echo "also pogchamp"
