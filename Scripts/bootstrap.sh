0f79793abcd79ad196f1823f377031add2b3c38a
Emile Reiser <emile.reiser@outlook.co.nz>
2021-07-28 16:43:02 +1200

    removed comments so it actually works

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -2,15 +2,16 @@
 
 SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
 
+# Zsh Setup
+ln -s $SCRIPTDIR/../zsh/.zshenv $HOME
+
 # Theme sublime text with pywal
 SUBLIMEDIR="$HOME/.config/sublime-text/Packages/User"
 if [ -e "$SUBLIMEDIR/Preferences.sublime-settings" ] && [ ! -L "$SUBLIMEDIR/Preferences.sublime-settings" ]; then
-	#curl -o $HOME/.local/bin/sublime-pywal https://raw.githubusercontent.com/Brychlikov/pywal_sublime/master/pywal_sublime.py
-	#sed -i -e "s/sublime-text-3/sublime-text/g" $HOME/.local/bin/sublime-pywal
-	#chmod +x $HOME/.local/bin/sublime-pywal
-	#rm $HOME/.config/sublime-text/Packages/User/Preferences.sublime-settings
-	#ln -s $SCRIPTDIR/../config/sublime-text/Preferences.sublime-settings $HOME/.config/sublime-text/Packages/User
-	echo "pogchamp"
+	curl -o $HOME/.local/bin/sublime-pywal https://raw.githubusercontent.com/Brychlikov/pywal_sublime/master/pywal_sublime.py
+	sed -i -e "s/sublime-text-3/sublime-text/g" $HOME/.local/bin/sublime-pywal
+	chmod +x $HOME/.local/bin/sublime-pywal
+	rm $HOME/.config/sublime-text/Packages/User/Preferences.sublime-settings
+	ln -s $SCRIPTDIR/../config/sublime-text/Preferences.sublime-settings $HOME/.config/sublime-text/Packages/User
 fi
 
-echo "also pogchamp"
