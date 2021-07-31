266d7210cfa3cbdd86201a75e146af1e1178b6d0
Emile Reiser <emile.reiser@outlook.co.nz>
2021-07-31 22:28:11 +1200

    created package install script

---
+++ Scripts/install_packages.sh
@@ -0,0 +1,7 @@
+$WEB=(firefox torbrowser-launcher protonvpn transmission-gtk)
+$UTILITIES=(alsa-utils htop ranger p7zip unrar tesseract tesseract-data-eng timeshift zsh ntfs-3g)
+$MEDIA=(blender sxiv vlc)
+$COMMUNICATION=(discord thunderbird)
+$DOCUMENT=(libreoffice-fresh texlive-core antiword docx2txt sublime-text-4)
+$PENTEST=(binwalk burpsuite gdb gnu-netcat gobuster-bin steghide wordlists sqlmap rustscan nmap impacket radare2 rlwrap hexedit r2ghidra hydra john-git metasploit nikto whois)
+$THEMING=(lxappearance python-pywal)
