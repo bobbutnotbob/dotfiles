c6e7d6625827d4e1c772ffc5e1b624f918d933e7
Emile Reiser <emile.reiser@outlook.co.nz>
2021-07-31 22:32:07 +1200

    made it so they actually get installed lol

--- Scripts/install_packages.sh
+++ Scripts/install_packages.sh
@@ -5,3 +5,13 @@ $COMMUNICATION=(discord thunderbird)
 $DOCUMENT=(libreoffice-fresh texlive-core antiword docx2txt sublime-text-4)
 $PENTEST=(binwalk burpsuite gdb gnu-netcat gobuster-bin steghide wordlists sqlmap rustscan nmap impacket radare2 rlwrap hexedit r2ghidra hydra john-git metasploit nikto whois)
 $THEMING=(lxappearance python-pywal)
+
+# Installs packages in sets
+# Comment out a line if you don't want certain packages
+yay -S $WEB
+yay -S $UTILITIES
+yay -S $MEDIA
+yay -S $COMMUNICATION
+yay -S $DOCUMENT
+yay -S $PENTEST
+yay -S $THEMING
